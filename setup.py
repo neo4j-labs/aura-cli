4dfefa910a15aaa982ad49701682d20becb2158f
Daniel Ruminski <daniel.ruminski@neotechnology.com>
2023-08-19 17:09:37 +0100

    Add requirements.txt files

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 from setuptools import find_packages, setup
 
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+
+with open("test-requirements.txt") as f:
+    test_requirements = f.read().splitlines()
+
 setup(
     name="aura-cli",
     version="0.2.3",
     packages=find_packages(),
     include_package_data=True,
-    install_requires=["click", "requests"],
+    install_requires=requirements,
+    tests_require=test_requirements,
     entry_points="""
         [console_scripts]
         aura=aura.aura:cli
